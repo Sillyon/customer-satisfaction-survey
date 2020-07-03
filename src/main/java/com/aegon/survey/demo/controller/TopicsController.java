05ca11ef5d15a135f5deabec9282cb2b8f3fe57a
Sillyon <comakfatihh@gmail.com>
2020-07-03 15:17:34 +0300

    rest class updated.

--- src/main/java/com/aegon/survey/demo/controller/TopicsController.java
+++ src/main/java/com/aegon/survey/demo/controller/TopicsController.java
@@ -3,7 +3,11 @@ package com.aegon.survey.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,10 +30,28 @@ public class TopicsController {
 		return service.saveTopics(topics);
 	}
 	
+	@GetMapping("/topics")
 	public List<Topics> findAllTopics(){
 		return service.getTopics();
 	}
 	
+	@GetMapping("/topics/{id}")
+	public Topics findTopicById(@PathVariable int id) {
+		return service.getTopicById(id);
+	}
 	
+	@GetMapping("/topics/{name}")
+	public Topics findTopicByName(@PathVariable String name) {
+		return service.getTopicByName(name);
+	}
 	
+	@PutMapping("/update")
+	public Topics updateTopic(@RequestBody Topics topic) {
+		return service.saveTopic(topic);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteTopicById(@PathVariable int id) {
+		return service.deleteTopic(id);
+	}
 }
