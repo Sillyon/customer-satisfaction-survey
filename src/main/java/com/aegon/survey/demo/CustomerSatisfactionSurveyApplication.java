88272ce038730d6ce7b098371e4dc29fadeb0908
Sillyon <comakfatihh@gmail.com>
2020-07-03 08:45:58 +0300

    init project

---
+++ src/main/java/com/aegon/survey/demo/CustomerSatisfactionSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.aegon.survey.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSatisfactionSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSatisfactionSurveyApplication.class, args);
+	}
+
+}
