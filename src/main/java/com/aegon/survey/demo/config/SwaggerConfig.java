553c699fa982d46b399e63eef58f6e606d5276a7
Sillyon <comakfatihh@gmail.com>
2020-07-04 23:24:20 +0300

    swagger dependency added

---
+++ src/main/java/com/aegon/survey/demo/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.aegon.survey.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Post API Reference")
+                .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select().paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.aegon.survey.demo"))
+                .build()
+                .pathMapping("/")
+                .useDefaultResponseMessages(false)
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class);
+    }
+}
