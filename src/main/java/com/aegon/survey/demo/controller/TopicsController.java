6aa807df3c7a33bfaa07fe2e66fce97968439de4
Sillyon <comakfatihh@gmail.com>
2020-07-04 17:32:36 +0300

    CRUD updated.

--- src/main/java/com/aegon/survey/demo/controller/TopicsController.java
+++ src/main/java/com/aegon/survey/demo/controller/TopicsController.java
@@ -20,27 +20,27 @@ public class TopicsController {
 	@Autowired
 	private TopicsService service;
 	
-	@PostMapping("/addtopic")
+	@PostMapping("/addTopic")
 	public Topics addTopic(@RequestBody Topics topic) {
 		return service.saveTopic(topic);
 	}
 	
-	@PostMapping("/addtopics")
+	@PostMapping("/addTopics")
 	public List<Topics> addTopics(@RequestBody List<Topics> topics) {
 		return service.saveTopics(topics);
 	}
 	
 	@GetMapping("/topics")
-	public List<Topics> findAllTopics(){
+	public List<Topics> findAllTopics() {
 		return service.getTopics();
 	}
 	
-	@GetMapping("/topics/{id}")
+	@GetMapping("/topicById/{id}")
 	public Topics findTopicById(@PathVariable int id) {
 		return service.getTopicById(id);
 	}
 	
-	@GetMapping("/topics/{name}")
+	@GetMapping("/topicByName/{name}")
 	public Topics findTopicByName(@PathVariable String name) {
 		return service.getTopicByName(name);
 	}
