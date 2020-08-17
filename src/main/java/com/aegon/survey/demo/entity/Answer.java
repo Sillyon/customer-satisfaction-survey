6c35119d756dec4f4820805d34636185b974e452
Sillyon <comakfatihh@gmail.com>
2020-08-17 03:57:16 +0300

    fix id sequence

--- src/main/java/com/aegon/survey/demo/entity/Answer.java
+++ src/main/java/com/aegon/survey/demo/entity/Answer.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 public class Answer {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int submitId;
 	
 	private int score;
